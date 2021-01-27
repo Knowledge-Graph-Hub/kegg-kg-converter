3f342ca6175ba65cb8324119416b1bda9eebae27
Harshad <hrshdhgd@users.noreply.github.com>
2021-01-26 20:06:25 -0600

    added edges

--- kg_converter/transform_utils/kegg/kegg.py
+++ kg_converter/transform_utils/kegg/kegg.py
@@ -22,7 +22,7 @@ Data of interest:
 class KEGGTransform(Transform):
 
     def __init__(self, input_dir: str = None, output_dir: str = None, nlp = False) -> None:
-        source_name = "kegg"
+        source_name = 'kegg'
         super().__init__(source_name, input_dir, output_dir, nlp)  # set some variables
 
         self.node_header = ['id', 'name', 'category']
@@ -52,8 +52,6 @@ class KEGGTransform(Transform):
 
         node_dict, edge_dict = self.post_data(self.cpd_path_link, node_dict, edge_dict)
         #node_dict, edge_dict = self.post_data(self.cpd_rn_link, node_dict, edge_dict)
-
-        import pdb; pdb.set_trace()
                     
 
         return None
@@ -65,32 +63,55 @@ class KEGGTransform(Transform):
                 open(self.output_edge_file, 'w') as edge:
 
                 # write headers (change default node/edge headers if necessary
-                node.write("\t".join(self.node_header) + "\n")
-                edge.write("\t".join(self.edge_header) + "\n")
+                node.write('\t'.join(self.node_header) + '\n')
+                edge.write('\t'.join(self.edge_header) + '\n')
                 
                 seen_node: dict = defaultdict(int)
                 seen_edge: dict = defaultdict(int)
 
                 # Nodes
-                cpd_node_type = "biolink:ChemicalSubstance"
-                path_node_type = "biolink:Pathway"
-                rn_node_type = "biolink:MolecularActivity"
-                ko_node_type = "biolink:GeneFamily"
+                cpd_node_type = 'biolink:ChemicalSubstance'
+                path_node_type = 'biolink:Pathway'
+                rn_node_type = 'biolink:MolecularActivity'
+                ko_node_type = 'biolink:GeneFamily'
 
                 # Edges
-                cpd_to_path_label = "biolink:ChemicalToPathwayAssociation"
-                cpd_to_rn_label = "#_ChemicalToReactionAssociaton"
-                path_to_rn_label = "#_PathToReactionAssociation"
+                cpd_to_path_label = 'biolink:ChemicalToPathwayAssociation'
+                cpd_to_rn_label = '#_ChemicalToReactionAssociaton'
+                path_to_rn_label = '#_PathToReactionAssociation'
+                ko_to_path_label = ''
+                ko_to_rn_label = ''
+                predicate = ''
+                predicate_curie = ''
+                edge_id = ''
+                subject = ''
+                object = ''
+
+                cpd_to_path_relation = 'NEED_CURIE'
+                cpd_to_rn_relation = 'NEED_CURIE'
+                path_to_rn_relation = 'NEED_CURIE'
+                ko_to_path_relation = 'NEED_CURIE'
+                ko_to_rn_relation = 'NEED_CURIE'
 
                 header_items = parse_header(f.readline(), sep='\t')
 
-                # Nodes
+                if all(x in header_items for x in ['cpdId', 'pathwayId']):
+                    predicate = cpd_to_path_label
+                    predicate_curie = cpd_to_path_relation
+                elif all(x in header_items for x in ['cpdId', 'rnId']):
+                    predicate = cpd_to_rn_label
+                    predicate_curie = cpd_to_rn_relation
+                elif all(x in header_items for x in ['pathwayId', 'rnId']):
+                    predicate = path_to_rn_label
+                    predicate_curie = path_to_rn_relation
+                
                 for line in f:
                     # transform line into nodes and edges
                     # node.write(this_node1)
                     # node.write(this_node2)
                     # edge.write(this_edge)
                     items_dict = parse_line(line, header_items, sep='\t')
+                    
                     
                     for key in items_dict.keys():
                         list_df = pd.DataFrame()
@@ -108,20 +129,34 @@ class KEGGTransform(Transform):
                             list_df = pd.read_csv(self.ko_list, sep='\t', low_memory=False)
                             node_type = ko_node_type
 
-                        id = items_dict[key]
-                        name = list_df[list_df[key] == id][key[:-2]].values[0]
+                        node_id = items_dict[key]
 
-                        if id not in seen_node:
+                        if edge_id == '':
+                            edge_id = node_id
+                            subject = node_id
+                        else:
+                            edge_id += node_id
+                            object = node_id
+
+                        name = list_df[list_df[key] == node_id][key[:-2]].values[0]
+                        # Nodes
+                        if node_id not in seen_node:
                             write_node_edge_item(fh=node,
                                                 header=self.node_header,
-                                                data=[id,
+                                                data=[node_id,
                                                       name,
                                                       node_type])
-                        seen_node[id] += 1
+                            seen_node[node_id] += 1
 
-                        
-
-
+                    # Edges
+                    if edge_id not in seen_edge:
+                        write_node_edge_item(fh=edge,
+                                        header=self.edge_header,
+                                        data=[subject,
+                                            predicate,
+                                            object,
+                                            predicate_curie])
+                        seen_edge[edge_id] += 1
 
         return [seen_node, seen_edge]
 
