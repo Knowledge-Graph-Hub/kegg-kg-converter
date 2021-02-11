86b4b382d4f163cce28e8a1509b38422e8c9dd1b
Harshad <hrshdhgd@users.noreply.github.com>
2021-02-11 17:28:08 -0600

    updated gitignore, merge.yaml and test_kegg

--- tests/test_kegg.py
+++ tests/test_kegg.py
@@ -13,14 +13,15 @@ class TestKEGG(unittest.TestCase):
         self.resources = 'tests/resources/'
         self.input_dir = os.path.join(self.resources,'kegg/input/')
         self.output_dir = os.path.join(self.resources,'kegg/output/')
-        self.kegg_fh = open(os.path.join(self.input_dir, 'rt'))
+        self.test_file = 'kegg_test.tsv'
+        self.kegg_fh = open(os.path.join(self.input_dir, self.test_file),'rt')
         self.kegg_output_dir = os.path.join(self.output_dir, "kegg/")
         self.kegg = KEGGTransform(input_dir=self.input_dir,
                                                  output_dir=self.output_dir)
 
     @parameterized.expand([
-     ('subject_id', '1'),
-     ('object_id', '2'),
+     ('subjectId', 'path:map00010'),
+     ('objectId', 'cpd:C00022'),
      ])
     def test_parse_kegg_line(self, key, value):
         header = parse_header(self.kegg_fh.readline())
@@ -31,29 +32,20 @@ class TestKEGG(unittest.TestCase):
 
     def test_run(self):
         self.assertTrue(isinstance(self.kegg.run, object))
-        self.kegg.run(data_file='drug.target.interaction_SNIPPET.tsv.gz')
+        self.kegg.run(data_file=self.test_file)
         self.assertTrue(os.path.isdir(self.kegg_output_dir))
 
     def test_nodes_file(self):
-        self.kegg.run(data_file='drug.target.interaction_SNIPPET.tsv.gz')
+        self.kegg.run(data_file=self.test_file)
         node_file = os.path.join(self.kegg_output_dir, "nodes.tsv")
         self.assertTrue(os.path.isfile(node_file))
         node_df = pd.read_csv(node_file, sep="\t", header=0)
         self.assertEqual((23, 5), node_df.shape)
-        self.assertEqual(['id', 'name', 'category', 'TDL', 'provided_by'],
+        self.assertEqual(['id', 'name', 'category', 'exact_match', 'description' ],
                          list(node_df.columns))
-        self.assertListEqual(['DrugCentral:4', 'UniProtKB:P35499', 'UniProtKB:P10635',
-                              'UniProtKB:Q12809', 'UniProtKB:Q9UK17', 'UniProtKB:P34995',
-                              'UniProtKB:P35498', 'UniProtKB:P22460', 'UniProtKB:P46098',
-                              'DrugCentral:5', 'UniProtKB:Q01668', 'UniProtKB:Q13936',
-                              'DrugCentral:6', 'UniProtKB:O15554', 'UniProtKB:O60840',
-                              'DrugCentral:38', 'UniProtKB:O15399', 'UniProtKB:O60391',
-                              'UniProtKB:Q05586', 'UniProtKB:Q12879', 'UniProtKB:Q13224',
-                              'UniProtKB:Q14957', 'UniProtKB:Q8TCU5'],
-                              list(node_df.id.unique()))
 
     def test_nodes_are_not_repeated(self):
-        self.kegg.run(data_file='drug.target.interaction_SNIPPET.tsv.gz')
+        self.kegg.run(data_file=self.test_file)
         node_file = os.path.join(self.kegg_output_dir, "nodes.tsv")
         node_df = pd.read_csv(node_file, sep="\t", header=0)
         nodes = list(node_df.id)
@@ -61,12 +53,12 @@ class TestKEGG(unittest.TestCase):
         self.assertCountEqual(nodes, unique_nodes)
 
     def test_edges_file(self):
-        self.kegg.run(data_file='drug.target.interaction_SNIPPET.tsv.gz')
+        self.kegg.run(data_file=self.test_file)
         edge_file = os.path.join(self.dc_output_dir, "edges.tsv")
         self.assertTrue(os.path.isfile(edge_file))
         edge_df = pd.read_csv(edge_file, sep="\t", header=0)
         self.assertEqual((21, 7), edge_df.shape)
         self.assertEqual(
-            ['subject', 'edge_label', 'object', 'relation', 'provided_by', 'comment', 'type'],
+            ['subject', 'predicate', 'object', 'relation'],
              list(edge_df.columns)
         )
